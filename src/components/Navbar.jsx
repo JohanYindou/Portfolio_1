c30ed2aabdcb1e30fdee6221fcada177acceff5f
Johan Yindou <jyindou@gmail.com>
2024-02-07 20:22:31 +0100

    Ajout du repo du projet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,51 +32,47 @@ function NavBar() {
   return (
     <Navbar
       expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
+      fixed='top'
+      expand='md'
+      className={navColour ? 'sticky' : 'navbar'}>
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={Icon} className="img-fluid logo" alt="brand" />
+        <Navbar.Brand href='/' className='d-flex'>
+          <img src={Icon} className='img-fluid logo' alt='brand' />
         </Navbar.Brand>
         <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
+          aria-controls='responsive-navbar-nav'
           onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
+            updateExpanded(expand ? false : 'expanded');
+          }}>
           <span></span>
           <span></span>
           <span></span>
         </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='ms-auto' defaultActiveKey='#home'>
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              <Nav.Link as={Link} to='/' onClick={() => updateExpanded(false)}>
+                <AiOutlineHome style={{ marginBottom: '2px' }} /> Home
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                to='/about'
+                onClick={() => updateExpanded(false)}>
+                <AiOutlineUser style={{ marginBottom: '2px' }} /> About
               </Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
+                to='/project'
+                onClick={() => updateExpanded(false)}>
                 <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
+                  style={{ marginBottom: '2px' }}
+                />{' '}
                 Projects
               </Nav.Link>
             </Nav.Item>
@@ -84,21 +80,19 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                to='/resume'
+                onClick={() => updateExpanded(false)}>
+                <CgFileDocument style={{ marginBottom: '2px' }} /> Resume
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item className="fork-btn">
+            <Nav.Item className='fork-btn'>
               <Button
-                href="" // Mettre le repo du projet
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
+                href='https://github.com/JohanYindou/Portfolio_1' 
+                target='_blank'
+                className='fork-btn-inner'>
+                <CgGitFork style={{ fontSize: '1.2em' }} />{' '}
+                <AiFillStar style={{ fontSize: '1.1em' }} />
               </Button>
             </Nav.Item>
           </Nav>
