982f4e5c170bc629dd19d7ecb91a4a112bc4b996
Johan Yindou <jyindou@gmail.com>
2024-02-07 20:15:30 +0100

    Ajout du CV

--- src/components/Resume/ResumeNews.jsx
+++ src/components/Resume/ResumeNews.jsx
@@ -6,6 +6,7 @@ import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import CV from '../../Assets/CV.pdf';
 
 
 function ResumeNew() {
@@ -22,7 +23,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button
             variant='primary'
-            href={} // Lien CV en PDF
+            href={CV} 
             target='_blank'
             style={{ maxWidth: '250px' }}>
             <AiOutlineDownload />
@@ -31,8 +32,7 @@ function ResumeNew() {
         </Row>
 
         <Row className='resume'>
-        {/*Lien CV en PDF*/}
-          <Document file={} className='d-flex justify-content-center'>
+          <Document file={CV} className='d-flex justify-content-center'>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -40,7 +40,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button
             variant='primary'
-            href={} // Lien CV en PDF
+            href={CV} 
             target='_blank'
             style={{ maxWidth: '250px' }}>
             <AiOutlineDownload />
