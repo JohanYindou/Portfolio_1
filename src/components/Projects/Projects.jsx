94fcaa34ce1a690b62c37f3528f45131e0cba3be
Johan Yindou <jyindou@gmail.com>
2024-02-07 20:10:29 +0100

    Ajout de quelques projets

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import ProjectCard from './ProjectCards';
+import ProjectCard from './ProjectsCards';
 import Particle from '../Particle';
+import Countrix from './media/Countrix_1.png';
+import Cinemax from './media/Cinemax_1.png';
+import Supakar from './media/Supakar_1.png';
 
 
 function Projects() {
@@ -18,19 +21,38 @@ function Projects() {
         <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
           <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={} // Image path here
+              imgPath={Countrix}
               isBlog={false}
-              title='' // Titre
-              description='' // Description
-              ghLink='' // Lien Github
-              demoLink='' // Lien Demo
+              title='Countrix'
+              description="Countrix est une application Web qui sert d'interface pour l'api RestCountries. Projet réalisé dans un but d'apprentissage."
+              ghLink='https://github.com/JohanYindou/Countrix'
+              // demoLink=''
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={Cinemax}
+              isBlog={false}
+              title='Cinemax'
+              description="Cinemax est une app de recherche de film et permet d'ajouter une sélection en favoris réalisé dans un but d'aprentissage."
+              ghLink='https://github.com/JohanYindou/Cinemax'
+              // demoLink=''
+            />
+          </Col>
+          <Col md={4} className='project-card'>
+            <ProjectCard
+              imgPath={Supakar}
+              isBlog={false}
+              title='Supakar'
+              description="Cette application de personnalisation de voitures, intégrée avec React à partir d'un modèle préexistant, vise à offrir une expérience interactive tout en servant de projet d'apprentissage pour la maîtrise de React"
+              ghLink='https://github.com/JohanYindou/Supakar'
+              // demoLink=''
             />
           </Col>
 
-        {/*
+          {/*
             Rajouter outant de Col que de projets 
         */}
-                  
         </Row>
       </Container>
     </Container>
