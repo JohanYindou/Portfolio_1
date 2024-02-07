9dc07fbfe1a46f6d7470fbdcc0700a801d743105
Johan Yindou <jyindou@gmail.com>
2024-02-07 20:54:23 +0100

    Modification des logos

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { CgCPlusPlus } from 'react-icons/cg';
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
+  DiSymfony,
+  DiPhp,
+  DiHtml5,
+  DiCss3,
 } from 'react-icons/di';
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
+  SiExpress,
 } from 'react-icons/si';
-import { TbBrandGolang } from 'react-icons/tb';
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
       <Col xs={4} md={2} className='tech-icons'>
-        <CgCPlusPlus />
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons'>
+        <DiCss3 />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
         <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <TbBrandGolang />
+        <DiPhp />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <DiReact />
+        <SiExpress />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <SiSolidity />
+        <DiReact />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
         <DiMongodb />
@@ -47,22 +47,10 @@ function Techstack() {
         <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
         <SiPostgresql />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className='tech-icons'>
-        <DiJava />
+        <DiSymfony />
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.jsx
+++ src/components/About/Toolstack.jsx
@@ -3,17 +3,16 @@ import { Col, Row } from 'react-bootstrap';
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiSlack,
+  SiGithub,
   SiVercel,
-  SiMacos,
+  SiGit,
+  SiFigma,
+  SiDocker,
 } from 'react-icons/si';
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      <Col xs={4} md={2} className='tech-icons'>
-        <SiMacos />
-      </Col>
       <Col xs={4} md={2} className='tech-icons'>
         <SiVisualstudiocode />
       </Col>
@@ -21,10 +20,19 @@ function Toolstack() {
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
-        <SiSlack />
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons'>
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons'>
+        <SiGit />
       </Col>
       <Col xs={4} md={2} className='tech-icons'>
         <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons'>
+        <SiDocker />
       </Col>
     </Row>
   );
