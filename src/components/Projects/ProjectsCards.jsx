cc8844d0aa776278d806aea0aa28a2dac5bcc08a
Johan Yindou <jyindou@gmail.com>
2024-02-09 11:46:16 +0100

    Added technology tags to projects.

--- src/components/Projects/ProjectsCards.jsx
+++ src/components/Projects/ProjectsCards.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import Stack from 'react-bootstrap/Stack';
 import { CgWebsite } from 'react-icons/cg';
 import { BsGithub } from 'react-icons/bs';
 
@@ -9,9 +11,17 @@ function ProjectCards(props) {
     <Card className='project-card-view'>
       <Card.Img variant='top' src={props.imgPath} alt='card-img' />
       <Card.Body>
-        {/** Rajouter un tag de la technologie utilisée */ }
-        
         <Card.Title>{props.title}</Card.Title>
+        {/** Tags de technologie */}
+        <Stack direction='horizontal' className='mt-3 mb-4' gap={2} spacing={2}>
+          {props.technologies.map((technology, index) => (
+            <span
+              key={index}
+              className='badge rounded-pill btn-primary btn-primary:hover p-2'>
+              {technology}
+            </span>
+          ))}
+        </Stack>
         <Card.Text style={{ textAlign: 'justify' }}>
           {props.description}
         </Card.Text>
