9ef47ccbbc80869948dc6b66ae71fdad9216e1a8
Johan Yindou <jyindou@gmail.com>
2024-11-21 13:13:15 +0100

    Modification de la section Résumé avec supression du CV et ajout de 2 CV pour chaque cas

--- src/components/Resume/ResumeNews.jsx
+++ src/components/Resume/ResumeNews.jsx
@@ -6,7 +6,9 @@ import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-import CV from '../../assets/CV.pdf';
+import CV from '../../assets/CV_Johan_Yindou Alternance.pdf';
+import CV1 from '../../assets/CV_Johan_Yindou Master.pdf';
+
 
 
 function ResumeNew() {
@@ -20,6 +22,7 @@ function ResumeNew() {
     <div>
       <Container fluid className='resume-section'>
         <Particle />
+        <h2 className='fw-bold'>CV pour les entreprises</h2>
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button
             variant='primary'
@@ -30,22 +33,26 @@ function ResumeNew() {
             &nbsp;Télécharger mon CV
           </Button>
         </Row>
-
         <Row className='resume'>
           <Document file={CV} className='d-flex justify-content-center'>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-
+        <h2 className='fw-bold'>CV pour les écoles et formations</h2>
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button
             variant='primary'
-            href={CV}
+            href={CV1}
             target='_blank'
             style={{ maxWidth: '250px' }}>
             <AiOutlineDownload />
             &nbsp;Télécharger mon CV
           </Button>
+        </Row>
+        <Row className='resume'>
+          <Document file={CV1} className='d-flex justify-content-center'>
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
       </Container>
     </div>
