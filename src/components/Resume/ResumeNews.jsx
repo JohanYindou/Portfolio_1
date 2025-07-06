5f6c06067916e31a98611a84863ef3dec7e08ef5
Johan Yindou <jyindou@gmail.com>
2025-07-06 21:07:13 +0200

    Update CV

--- src/components/Resume/ResumeNews.jsx
+++ src/components/Resume/ResumeNews.jsx
@@ -6,8 +6,7 @@ import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-import CV from '../../assets/CV_Johan_Yindou Alternance.pdf';
-import CV1 from '../../assets/CV_Johan_Yindou Master.pdf';
+import CV from '../../assets/Johan_Yindou_CV.pdf';
 
 
 
@@ -22,7 +21,7 @@ function ResumeNew() {
     <div>
       <Container fluid className='resume-section'>
         <Particle />
-        <h2 className='fw-bold'>CV pour les entreprises</h2>
+        <h2 className='fw-bold'>Curriculum Vitae</h2>
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button
             variant='primary'
@@ -35,22 +34,6 @@ function ResumeNew() {
         </Row>
         <Row className='resume'>
           <Document file={CV} className='d-flex justify-content-center'>
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-        <h2 className='fw-bold'>CV pour les écoles et formations</h2>
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button
-            variant='primary'
-            href={CV1}
-            target='_blank'
-            style={{ maxWidth: '250px' }}>
-            <AiOutlineDownload />
-            &nbsp;Télécharger mon CV
-          </Button>
-        </Row>
-        <Row className='resume'>
-          <Document file={CV1} className='d-flex justify-content-center'>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
